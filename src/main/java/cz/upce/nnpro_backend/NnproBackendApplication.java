31c2d67487866c2f0c43171afd80e30a0a9a6f35
DOL7JS <petan.dolejs7@seznam.cz>
2023-09-21 18:11:00 +0200

    Set db connection

--- src/main/java/cz/upce/nnpro_backend/NnproBackendApplication.java
+++ src/main/java/cz/upce/nnpro_backend/NnproBackendApplication.java
@@ -2,11 +2,9 @@ package cz.upce.nnpro_backend;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @SpringBootApplication
 @SecurityScheme(name = "NNPRO_API", scheme = "Bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
