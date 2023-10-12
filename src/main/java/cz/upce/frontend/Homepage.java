c2326972bc42df48869dd483842739eb81470c0c
DOL7JS <petan.dolejs7@seznam.cz>
2023-10-12 18:13:39 +0200

    Added car image to Homepage.java

--- src/main/java/cz/upce/frontend/Homepage.java
+++ src/main/java/cz/upce/frontend/Homepage.java
@@ -4,9 +4,12 @@ import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
+import com.vaadin.flow.server.VaadinService;
 
 import javax.annotation.security.PermitAll;
 
@@ -16,19 +19,28 @@ import javax.annotation.security.PermitAll;
 public class Homepage extends Composite<VerticalLayout> {
 
     public Homepage() {
-        VerticalLayout layoutColumn2 = new VerticalLayout();
+        VerticalLayout verticalLayout = new VerticalLayout();
         H1 h1 = new H1();
         getContent().setHeightFull();
         getContent().setWidthFull();
-        getContent().setFlexGrow(1.0, layoutColumn2);
-        layoutColumn2.setWidthFull();
+        getContent().setFlexGrow(1.0, verticalLayout);
+        verticalLayout.setWidthFull();
         h1.setText("Vítejte na stránkách CRV");
-        layoutColumn2.setFlexGrow(1.0, h1);
-        layoutColumn2.setAlignSelf(FlexComponent.Alignment.CENTER, h1);
-        Image image = new Image("frontend/images/car.png","car");
-        getContent().add(layoutColumn2);
-        layoutColumn2.add(h1);
-        getContent().add(image);
+        verticalLayout.setFlexGrow(1.0, h1);
+        verticalLayout.setAlignSelf(FlexComponent.Alignment.CENTER, h1);
+        StreamResource resource = new StreamResource("car.png", () -> VaadinService.getCurrent().getClassLoader().getResourceAsStream("car.png"));
+        verticalLayout.add(h1);
+        getContent().add(verticalLayout);
+
+        Image image = new Image(resource, "Alternative Text");
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        horizontalLayout.add(image);
+        getContent().add(horizontalLayout);
+        getContent().setFlexGrow(1.0, horizontalLayout);
+        horizontalLayout.setWidthFull();
+
+        horizontalLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        horizontalLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
     }
 
 
